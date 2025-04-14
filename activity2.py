b49930807f70438ff1364eee9e8d23b796accb89
shourigit <shourisaha.codingal@gmail.com>
2025-04-15 01:56:03 +0600

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,11 @@
+# Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "Codingal"
+print(codingal)
+
+name = input("Enter your name: ")
+print("\nHello", name, "\n Welcome")
